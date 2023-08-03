a0805099975c318ea91c07b1fb481d9d2474a76b
Roysandrone1998 <raysandrone@gmail.com>
2023-08-02 23:04:29 -0300

    comentado

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,10 @@
+import Button from 'react-bootstrap/Button';
 function Header(){
     return(
-    <header className="">
+    <header className="bg-primary">
         <h1>Happy Skate.</h1>
         <p>Tu tienda online.</p>
+        <Button variant="dark">Inicio</Button>
     </header>
         )
 }
